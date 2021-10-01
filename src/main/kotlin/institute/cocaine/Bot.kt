f822d01285fd4457576fd20bfb5e2d18b3be6f55
Sanduhr32 <25187502+Sanduhr32@users.noreply.github.com>
2021-10-01 13:26:25 +0200

    audio #2

--- src/main/kotlin/institute/cocaine/Bot.kt
+++ src/main/kotlin/institute/cocaine/Bot.kt
@@ -1,7 +1,6 @@
 package institute.cocaine
 
 import dev.minn.jda.ktx.injectKTX
-import dev.minn.jda.ktx.interactions.choice
 import dev.minn.jda.ktx.interactions.option
 import dev.minn.jda.ktx.interactions.upsertCommand
 import dev.minn.jda.ktx.listener
@@ -10,6 +9,8 @@ import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.VoiceChannel
 import net.dv8tion.jda.api.events.ReadyEvent
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
 
 class Bot(private val token: String) {
     private val jda: JDA = JDABuilder.createLight(this.token).injectKTX().build()
@@ -18,22 +19,37 @@ class Bot(private val token: String) {
         jda.listener<ReadyEvent> { readyEvent ->
             val guild = jda.getGuildById("")!!
             guild.upsertCommand(name = "join", description = "Joins your current VC!") {
-                option<String>("channel", "joins another channel, you ain't at") {
-                    for (voiceChannel in guild.voiceChannelCache.take(24)) {
-                        choice(voiceChannel.name, voiceChannel.id)
-                    }
-                }
+                option<VoiceChannel>("channel", "joins another channel, you ain't at")
             }.queue()
         }
 
+        jda.listener<GuildVoiceJoinEvent> { event ->
+            val member = event.member
+            if (member == event.guild.selfMember)
+                return@listener
+        }
+
         jda.onCommand("join") { event ->
-            val id = event.getOption("channel")?.asString ?: return@onCommand
-            val channel = event.guild?.voiceChannelCache?.getElementById(id) ?: return@onCommand
-            event.deferReply().setContent("not yet in ${channel.name}").queue()
+            val argChannel = event.getOption("channel")?.asGuildChannel
+            val guild = event.guild ?: return@onCommand
+            val member = event.member ?: return@onCommand
+
+            val voiceState = member.voiceState
+
+            if (voiceState == null || !voiceState.inVoiceChannel() || voiceState.channel == null) {
+                if (argChannel == null || argChannel !is VoiceChannel) {
+                    return@onCommand
+                }
+                event.deferReply().queue()
+                return@onCommand
+            }
+            val channel = voiceState.channel!!
+            joinVC(event, channel)
         }
     }
 
-    suspend fun joinVC(vc: VoiceChannel) {
+    private fun joinVC(event: SlashCommandEvent, vc: VoiceChannel) {
         vc.guild.audioManager.openAudioConnection(vc)
+        event.reply("> Successfully joined you in ${vc.name}").queue()
     }
 }
